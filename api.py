c9b6e8e8692c294017f4ab52fd98836b97b39893
1Shadowscale1 <81811251+1Shadowscale1@users.noreply.github.com>
2022-06-06 00:50:44 +0500

    Solution added

---
+++ api.py
@@ -0,0 +1,42 @@
+import requests
+
+def getUsrId(usr):
+    try:
+        return requests.get(usr).text.split("money&target=mail")[1].split("&")[0]
+    except IndexError:
+        print("Closed profile")
+
+def getInfoFriend(id, token):
+    infoById = requests.get("https://api.vk.com/method/users.get?user_id=" +
+    id + "&" + "access_token=" + token + "&v=5.131").text.split('"')
+    return(infoById[7] + " " + infoById[11])
+
+
+def getStatus(id, token):
+    a = requests.get("https://api.vk.com/method/status.get?user_id=" +
+    id + "&" + "access_token=" + token + "&v=5.131").text.split("[")
+    return(a[0][21:-3])
+
+
+def getGroupsCount(id, token):
+    a = requests.get("https://api.vk.com/method/groups.get?user_id=" +
+    id + "&" + "access_token=" + token + "&v=5.131").text.split("[")
+    return(a[0][21:23])
+
+        
+def main():
+    token = "INSERT_YOUR_TOKEN_HERE"
+    usrInp = input("Write user id or user page link (for example https://vk.com/fqusl): ")
+    if len(usrInp.split("/")) <= 1:
+        usr = getInfoFriend(str(usrInp, token))
+        usrId = usr
+    else:
+        usr = getInfoFriend(getUsrId(str(usrInp)), token)
+        usrId = getUsrId(str(usrInp))
+
+    print("Status of " + str(usr) + ": " + getStatus(usrId, token) + "\n")
+    print("Amount of " + str(usr) + " groups: " + getGroupsCount(usrId, token))
+
+
+if __name__ == "__main__":
+    main()
